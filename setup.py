fd765c32670e950ff4e4508ab244a502e5a342dc
pointal <pointal@708f59d9-238d-437a-9a4d-2ddeb2e90e73>
2011-01-06 18:19:01 +0000

    Reorganized description files.

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(name='treetaggerwrapper',
     download_url='https://sourcesup.cru.fr/projects/ttpw/',
     description='Wrapper for the TreeTagger text annotation tool from H.Schmid.',
     package_dir={'': 'xxxxx'},
-    py_modules=['xxxxx'],
+    py_modules=['treetaggerwrapper.py'],
     provides=['xxxxx'],
     keywords=['tagger','treetagger','wrapper','text','annotation','linguistic'],
     license='GNU General Public License v2 or greater',
@@ -28,31 +28,6 @@ setup(name='treetaggerwrapper',
                 'Topic :: Scientific/Engineering',
                 'Topic :: Scientific/Engineering :: Information Analysis',
                 ],
-    long_description="""\
-Python wrapper for TreeTagger, a language independent part-of-speech tagger
----------------------------------------------------------------------------
-
-Wrap the Helmut Schmid tool into a Python class allowing to tag several texts
-one after the other, maintaining connexions with the tagger process to speed-up
-processing.
-
-Can do the chunking within the wrapper methods, or using an external tool (and
-provide directly tokens to the tagger).
-
-Using objects, can start multiple taggers simultaneously, eventually using
-different languages.
-
-Support chunking for:
-
-  - english
-  - french
-  - deutch
-  - spanish
-
-This version is based on Python 2.
-
-Treetagger itself is is freely available for research, education and evaluation.
-See http://www.ims.uni-stuttgart.de/projekte/corplex/TreeTagger/DecisionTreeTagger.html
-""",
+    long_description=open("README.txt").read(),
     )
 
