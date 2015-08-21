ec46493a8e527a26d3ae3be736661762dae720d8
pointal <pointal@708f59d9-238d-437a-9a4d-2ddeb2e90e73>
2015-08-21 21:06:26 +0000

    Version 2.0.5 for publish.

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ import io
 
 setup(
     name='treetaggerwrapper',
-    version='2.0.4',
+    version='2.0.5',
     author='Laurent Pointal',
     author_email='laurent.pointal@limsi.fr',
     url='http://perso.limsi.fr/pointal/dev:treetaggerwrapper',
