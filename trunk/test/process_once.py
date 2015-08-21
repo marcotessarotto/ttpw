1c54d0a2bd360560d6b1dd514b7cf628cce23d28
pointal <pointal@708f59d9-238d-437a-9a4d-2ddeb2e90e73>
2015-08-21 17:16:35 +0000

    Speed test with profiling.

--- trunk/test/process_once.py
+++ trunk/test/process_once.py
@@ -3,8 +3,13 @@
 from __future__ import print_function
 from __future__ import unicode_literals
 
+PROFILING = False
+
+if PROFILING:
+    import cProfile
 import treetaggerwrapper as ttpw
 import io
+import sys
 
 tt = ttpw.TreeTagger(LANG='fr')
 
@@ -13,5 +18,11 @@ with io.open("fileslist.txt", encoding="utf-8") as f:
 
 for fname in flist:
     print("Tagging:", fname)
-    tt.tag_file(fname)
-
+    if PROFILING:
+        cProfile.run("res = tt.tag_file(fname)")
+        break   # Just process one file.
+    else!
+        res = tt.tag_file(fname)
+        # Uncomment to save result and diff data.
+        #with io.open(fname+str(sys.version_info.major), "w", encoding='utf-8') as f:
+        #    f.writelines(res)
