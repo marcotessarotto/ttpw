31fda7cddca0c98babb5c7e790b0216b2df46604
pointal <pointal@708f59d9-238d-437a-9a4d-2ddeb2e90e73>
2015-08-21 09:18:29 +0000

    Catch dir access execption when searching for TreeTagger, hide console when starting TreeTagger on Windows. Version 2.0.2.

--- trunk/setup.py
+++ trunk/setup.py
@@ -8,7 +8,7 @@ import io
 
 setup(
     name='treetaggerwrapper',
-    version='2.0.1',
+    version='2.0.2',
     author='Laurent Pointal',
     author_email='laurent.pointal@limsi.fr',
     url='http://perso.limsi.fr/pointal/dev:treetaggerwrapper',
