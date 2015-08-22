a80e593af8b977b6efe64a99a25e2913e57ee357
pointal <pointal@708f59d9-238d-437a-9a4d-2ddeb2e90e73>
2015-08-22 13:03:35 +0000

    Go version 2.0.6.

--- trunk/treetaggerwrapper.py
+++ trunk/treetaggerwrapper.py
@@ -8,7 +8,7 @@ Python wrapper for TreeTagger
 :organization: CNRS - LIMSI
 :copyright: CNRS - 2004-2015
 :license: GNU-GPL Version 3 or greater
-:version: 2.0.5
+:version: 2.0.6
 
 For language independent part-of-speech tagger TreeTagger, 
 see `Helmut Schmid TreeTagger site`_.
@@ -313,7 +313,7 @@ from __future__ import unicode_literals
 # Note that use of sphinx 1.3 :any: role may broke epydoc (not tested).
 __docformat__ = "restructuredtext en"
 
-__version__ = '2.0.5'
+__version__ = '2.0.6'
 
 # Note: I use re.VERBOSE option everywhere to allow spaces and comments into
 #       regular expressions (more readable). And (?:...) allow to have
