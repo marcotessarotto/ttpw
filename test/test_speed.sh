202849c46b745d3f2bb3fd1752191b1ee46ed93d
pointal <pointal@708f59d9-238d-437a-9a4d-2ddeb2e90e73>
2015-08-25 08:17:51 +0000

    Simple time presentation in speed test.

--- test/test_speed.sh
+++ test/test_speed.sh
@@ -8,14 +8,14 @@ python process_once.py
 
 # Timed process
 echo "Using Python 2 with standard chunking:"
-time python process_once.py $1
+time -p python process_once.py $1
 
 echo "Using Python 3 with standard chunking:"
-time python3 process_once.py $1
+time -p python3 process_once.py $1
 
 # Timed process
 echo "Using Python 2 with external chunking:"
-time python process_once.py external $1
+time -p python process_once.py external $1
 
 echo "Using Python 3 with external chunking:"
-time python3 process_once.py external $1
+time -p python3 process_once.py external $1
